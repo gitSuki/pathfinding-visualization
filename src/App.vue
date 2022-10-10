f6b972d941afbc23a142aa41819d3df785205ee2
gitSuki <collinbrown226@gmail.com>
2022-10-09 23:09:09 -0500

    Create GridNode component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
 import { reactive } from "vue";
+import GridNode from "./components/GridNode.vue";
 
 // creating empty 2d grid
 const grid = reactive({
   cells: [],
   rows: 20,
-  cols: 30
+  cols: 50,
 });
 
 for (let i = 0; i < grid.rows; i++) {
@@ -17,7 +18,7 @@ for (let i = 0; i < grid.rows; i++) {
   <nav>Header</nav>
   <main>
     <div class="row" v-for="i in grid.rows">
-      <div class="col" v-for="j in grid.cols">{{ j }}</div>
+      <GridNode class="col" v-for="j in grid.cols" :row="i" :col="j"></GridNode>
     </div>
   </main>
 </template>
@@ -30,13 +31,20 @@ for (let i = 0; i < grid.rows; i++) {
   border: 1px solid red;
 }
 
+main {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding-left: 7.5px;
+  padding-right: 7.5px;
+}
+
 .row {
   display: flex;
 }
 
-.col {
+/* .col {
   background-color: lightgray;
   border-style:solid;
   border-width: 1px;
-}
+} */
 </style>
---
+++ src/components/GridNode.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { defineProps, computed } from "vue";
+
+// coordinate data
+const props = defineProps({
+  row: Number,
+  col: Number,
+});
+
+const rowCoordinate = computed(() => props.row - 1);
+const colCoordinate = computed(() => props.col - 1);
+
+function printCoordinate() {
+  console.log("Row: " + rowCoordinate.value + " Col: " + colCoordinate.value);
+}
+</script>
+
+<template>
+  <div @click="printCoordinate">Hi</div>
+</template>
