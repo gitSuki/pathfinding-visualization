f5686e77abffa8c94d00de1063d45f19c903b732
gitSuki <collinbrown226@gmail.com>
2022-10-17 13:43:42 -0500

    Add findClosest helper method for astar

--- src/algorithims/astar.js
+++ src/algorithims/astar.js
@@ -1,3 +1,39 @@
 export default function aStar(grid, startNode, finishNode) {
-  console.log(grid)
+  // returns a list of all visited nodes in the order they were visited
+  // all other nodes besides the startNode have their distance set to infinity
+  const closedSet = [];
+  const openSet = [];
+  startNode.distance = 0;
+  startNode.heuristic = 0;
+  startNode.totalDistance = 0;
+  openSet.push(startNode);
+
+  while (openSet.length > 0) {
+    let currNode = findClosest(openSet);
+    if (currNode === finishNode) return visitedNodesInOrder;
+  }
+}
+
+function findClosest(openSet) {
+  // looping through the openSet to find the closest node
+  let curr = openSet[0];
+  let closestNodeIndex = 0;
+  for (let i = 0; i < openSet.length; i++) {
+    // if curr is farther away than openSet[i]
+    if (curr.totalDistance > openSet[i].totalDistance) {
+      curr = openSet[i];
+      closestNodeIndex = i;
+    }
+    // if curr and openSet are the same distance, but curr's
+    // heuristic is farther away than openSet[i]'s
+    else if (
+      curr.totalDistance === openSet[i].totalDistance &&
+      curr.heuristic > openSet[i].heuristic
+    ) {
+      curr = openSet[i];
+      closestNodeIndex = i;
+    }
+  }
+  openSet.splice(closestNodeIndex, 1);
+  return curr;
 }
