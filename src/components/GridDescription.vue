203027e8a07584ec0c0d6fb36f6abeb3bff99152
gitSuki <collinbrown226@gmail.com>
2023-02-04 13:49:55 -0600

    Update the descripton component to include weighted node examples

--- src/components/GridDescription.vue
+++ src/components/GridDescription.vue
@@ -3,40 +3,46 @@ import GridNode from "./GridNode.vue";
 </script>
 
 <template>
-  <ul>
-    <li><grid-node :is-start="true"></grid-node>Start Node</li>
-    <li><grid-node :is-end="true"></grid-node>Target Node</li>
-    <li><grid-node :is-visited-anim="true"></grid-node>Visited Node</li>
-    <li>
-      <grid-node :is-shortest-path-anim="true"></grid-node>Shortest-path Node
-    </li>
-    <li><grid-node :is-wall="true"></grid-node>Wall Node</li>
-  </ul>
+    <div>
+        <ul>
+            <li><grid-node :weight="1"></grid-node>Weight 1 Node</li>
+            <li><grid-node :weight="3"></grid-node>Weight 3 Node</li>
+            <li><grid-node :weight="5"></grid-node>Weight 5 Node</li>
+            <li><grid-node :is-wall="true"></grid-node>Wall Node</li>
+        </ul>
+        <ul>
+            <li><grid-node :is-start="true"></grid-node>Start Node</li>
+            <li><grid-node :is-end="true"></grid-node>Target Node</li>
+            <li><grid-node :is-visited-anim="true"></grid-node>Visited Node</li>
+            <li><grid-node :is-shortest-path-anim="true"></grid-node>Shortest-path Node</li>
+            
+        </ul>
+    </div>
 </template>
 
 <style scoped>
 * {
-  outline: none !important;
-  animation: none !important;
+    outline: none !important;
+    animation: none !important;
 }
 
 ul {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-wrap: wrap;
-  list-style-type: none;
-  margin: 1.5rem 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    list-style-type: none;
+    margin: 1.5rem 1rem;
 }
 
 li {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 0.5rem;
-  font-size: 1.1rem;
-  margin: 0.25rem 1rem;
-  font-weight: bold;
-  color: #eeeeee;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 0.5rem;
+    font-size: 1.1rem;
+    margin: 0.25rem 1rem;
+    font-weight: bold;
+    color: #eeeeee;
 }
 </style>
