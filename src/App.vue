7c0bdd553f535eeba3d40231463affaf3c6c1794
gitSuki <collinbrown226@gmail.com>
2023-02-04 10:08:18 -0600

    Move the initialization width of the grip outside of createStore

--- src/App.vue
+++ src/App.vue
@@ -13,152 +13,146 @@ const store = useStore();
 const grid = store.getters.getGrid;
 
 for (let i = 0; i < grid.rows; i++) {
-  const currentRow = [];
-  for (let j = 0; j < grid.cols; j++) {
-    currentRow.push(createGridNodeObject(i, j));
-  }
-  grid.cells.push(currentRow);
+    const currentRow = [];
+    for (let j = 0; j < grid.cols; j++) {
+        currentRow.push(createGridNodeObject(i, j));
+    }
+    grid.cells.push(currentRow);
 }
 
 function createGridNodeObject(row, col) {
-  const randomWeights = [1, 3, 4]
-  return {
-    row: row,
-    col: col,
-    isStart:
-      store.getters.getStartNode.row === row &&
-      store.getters.getStartNode.col === col,
-    isEnd:
-      store.getters.getEndNode.row === row &&
-      store.getters.getEndNode.col === col,
-    isWall: false,
-    isVisited: false,
-    isVisitedAnim: false,
-    isShortestPathAnim: false,
-    isBeingDragged: false,
-    previousNode: null,
-    distanceSoFar: Infinity,
-    // random selects a value from the randomWeights array
-    weight: randomWeights[Math.floor(Math.random() * randomWeights.length)],
-  };
+    const randomWeights = [1, 3, 4];
+    return {
+        row: row,
+        col: col,
+        isStart: store.getters.getStartNode.row === row && store.getters.getStartNode.col === col,
+        isEnd: store.getters.getEndNode.row === row && store.getters.getEndNode.col === col,
+        isWall: false,
+        isVisited: false,
+        isVisitedAnim: false,
+        isShortestPathAnim: false,
+        isBeingDragged: false,
+        previousNode: null,
+        distanceSoFar: Infinity,
+        // random selects a value from the randomWeights array
+        weight: randomWeights[Math.floor(Math.random() * randomWeights.length)],
+    };
 }
 
 // running algorithims
 function runAlgo(grid, algoOption) {
-  store.dispatch("animRun");
-  const startNode =
-    grid[store.getters.getStartNode.row][store.getters.getStartNode.col];
-  const endNode =
-    grid[store.getters.getEndNode.row][store.getters.getEndNode.col];
+    store.dispatch("animRun");
+    const startNode = grid[store.getters.getStartNode.row][store.getters.getStartNode.col];
+    const endNode = grid[store.getters.getEndNode.row][store.getters.getEndNode.col];
 
-  if (algoOption === "djikstra") {
-    runDjikstra(grid, startNode, endNode);
-  } else {
-    runAstar(grid, startNode, endNode);
-  }
+    if (algoOption === "djikstra") {
+        runDjikstra(grid, startNode, endNode);
+    } else {
+        runAstar(grid, startNode, endNode);
+    }
 }
 
 function runAstar(grid, startNode, endNode) {
-  const closedSet = astarAlgo(grid, startNode, endNode);
-  const shortestPath = findShortestPath(endNode);
-  animateAlgo(closedSet, shortestPath);
+    const closedSet = astarAlgo(grid, startNode, endNode);
+    const shortestPath = findShortestPath(endNode);
+    animateAlgo(closedSet, shortestPath);
 }
 
 function runDjikstra(grid, startNode, endNode) {
-  const visitedNodesInOrder = djikstrasAlgo(grid, startNode, endNode);
-  const shortestPath = findShortestPath(endNode);
-  animateAlgo(visitedNodesInOrder, shortestPath);
+    const visitedNodesInOrder = djikstrasAlgo(grid, startNode, endNode);
+    const shortestPath = findShortestPath(endNode);
+    animateAlgo(visitedNodesInOrder, shortestPath);
 }
 
 function findShortestPath(endNode) {
-  const shortestPath = [];
-  let currNode = endNode;
-  while (currNode !== null) {
-    shortestPath.unshift(currNode);
-    currNode = currNode.previousNode;
-  }
-  return shortestPath;
+    const shortestPath = [];
+    let currNode = endNode;
+    while (currNode !== null) {
+        shortestPath.unshift(currNode);
+        currNode = currNode.previousNode;
+    }
+    return shortestPath;
 }
 
 function animateAlgo(visitedNodesInOrder, shortestPath) {
-  // creates the delayed animation effect that shows all the nodes
-  // that the search algorithm visited in order
-  for (let i = 0; i < visitedNodesInOrder.length; i++) {
-    if (i === visitedNodesInOrder.length - 1) {
-      setTimeout(() => {
-        animateShortestPath(shortestPath);
-        store.dispatch("toggleAnimResults", true);
-      }, 15 * i);
+    // creates the delayed animation effect that shows all the nodes
+    // that the search algorithm visited in order
+    for (let i = 0; i < visitedNodesInOrder.length; i++) {
+        if (i === visitedNodesInOrder.length - 1) {
+            setTimeout(() => {
+                animateShortestPath(shortestPath);
+                store.dispatch("toggleAnimResults", true);
+            }, 15 * i);
+        }
+        setTimeout(() => {
+            visitedNodesInOrder[i].isVisitedAnim = true;
+        }, 15 * i);
     }
-    setTimeout(() => {
-      visitedNodesInOrder[i].isVisitedAnim = true;
-    }, 15 * i);
-  }
 }
 
 function animateShortestPath(shortestPath) {
-  // creates the delayed animation effect to show the shortest path
-  // once the search algorithm has discovered the end node
-  for (let i = 0; i < shortestPath.length; i++) {
-    setTimeout(() => {
-      if (i === shortestPath.length - 1) store.dispatch("animEnd");
-      shortestPath[i].isVisitedAnim = false;
-      shortestPath[i].isShortestPathAnim = true;
-    }, 25 * i);
-  }
+    // creates the delayed animation effect to show the shortest path
+    // once the search algorithm has discovered the end node
+    for (let i = 0; i < shortestPath.length; i++) {
+        setTimeout(() => {
+            if (i === shortestPath.length - 1) store.dispatch("animEnd");
+            shortestPath[i].isVisitedAnim = false;
+            shortestPath[i].isShortestPathAnim = true;
+        }, 25 * i);
+    }
 }
 </script>
 
 <template>
-  <nav-bar @visualization="runAlgo"></nav-bar>
-  <grid-description></grid-description>
-  <main>
-    <div class="row" v-for="i in grid.rows">
-      <!-- v-for is 1-indexed -->
-      <grid-node
-        v-for="j in grid.cols"
-        :row="grid.cells[i - 1][j - 1].row"
-        :col="grid.cells[i - 1][j - 1].col"
-        :isStart="grid.cells[i - 1][j - 1].isStart"
-        :isEnd="grid.cells[i - 1][j - 1].isEnd"
-        :isWall="grid.cells[i - 1][j - 1].isWall"
-        :isVisited="grid.cells[i - 1][j - 1].isVisited"
-        :isVisitedAnim="grid.cells[i - 1][j - 1].isVisitedAnim"
-        :isShortestPathAnim="grid.cells[i - 1][j - 1].isShortestPathAnim"
-        :isBeingDragged="grid.cells[i - 1][j - 1].isBeingDragged"
-        :previousNode="grid.cells[i - 1][j - 1].previousNode"
-        :distanceSoFar="grid.cells[i - 1][j - 1].distanceSoFar"
-        :weight="grid.cells[i - 1][j - 1].weight"
-      ></grid-node>
-    </div>
-  </main>
+    <nav-bar @visualization="runAlgo"></nav-bar>
+    <grid-description></grid-description>
+    <main>
+        <div class="row" v-for="i in grid.rows">
+            <!-- v-for is 1-indexed -->
+            <grid-node
+                v-for="j in grid.cols"
+                :row="grid.cells[i - 1][j - 1].row"
+                :col="grid.cells[i - 1][j - 1].col"
+                :isStart="grid.cells[i - 1][j - 1].isStart"
+                :isEnd="grid.cells[i - 1][j - 1].isEnd"
+                :isWall="grid.cells[i - 1][j - 1].isWall"
+                :isVisited="grid.cells[i - 1][j - 1].isVisited"
+                :isVisitedAnim="grid.cells[i - 1][j - 1].isVisitedAnim"
+                :isShortestPathAnim="grid.cells[i - 1][j - 1].isShortestPathAnim"
+                :isBeingDragged="grid.cells[i - 1][j - 1].isBeingDragged"
+                :previousNode="grid.cells[i - 1][j - 1].previousNode"
+                :distanceSoFar="grid.cells[i - 1][j - 1].distanceSoFar"
+                :weight="grid.cells[i - 1][j - 1].weight"
+            ></grid-node>
+        </div>
+    </main>
 </template>
 
 <style>
 @font-face {
-  font-family: kiona-regular;
-  src: url(./assets/Kiona-Regular.ttf);
+    font-family: kiona-regular;
+    src: url(./assets/Kiona-Regular.ttf);
 }
 
 * {
-  font-family: kiona-regular, sans-serif;
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
+    font-family: kiona-regular, sans-serif;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
 }
 
 body {
-  background-color: #36393e;
+    background-color: #36393e;
 }
 
 main {
-  max-width: 1600px;
-  margin: 0 auto;
-  padding-left: 7.5px;
-  padding-right: 7.5px;
+    max-width: 1600px;
+    margin: 0 auto;
+    padding-left: 7.5px;
+    padding-right: 7.5px;
 }
 
 .row {
-  display: flex;
+    display: flex;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,95 +1,99 @@
 import { createStore } from "vuex";
 
+const initGridWidth = Math.round(screen.width / 27.5);
+const initStartNodeCol = Math.round((screen.width / 30) * 0.2);
+const initEndNodeCol = Math.round((screen.width / 30) * 0.8);
+
 export default createStore({
-  state() {
-    return {
-      grid: {
-        cells: [],
-        rows: 20,
-        cols: Math.round(screen.width / 27.5),
-      },
-      startNode: {
-        row: 9,
-        col: Math.round((screen.width / 30) * 0.2),
-      },
-      endNode: {
-        row: 9,
-        col: Math.round((screen.width / 30) * 0.8),
-      },
-      draggedNode: null,
-      isAnimRunning: false,
-      isResultsDisplayed: false,
-      isDropdownDisplayed: false,
-    };
-  },
-  getters: {
-    getGrid(state) {
-      return state.grid;
+    state() {
+        return {
+            grid: {
+                cells: [],
+                rows: 20,
+                cols: initGridWidth,
+            },
+            startNode: {
+                row: 9,
+                col: initStartNodeCol,
+            },
+            endNode: {
+                row: 9,
+                col: initEndNodeCol,
+            },
+            draggedNode: null,
+            isAnimRunning: false,
+            isResultsDisplayed: false,
+            isDropdownDisplayed: false,
+        };
     },
-    getStartNode(state) {
-      return state.startNode;
+    getters: {
+        getGrid(state) {
+            return state.grid;
+        },
+        getStartNode(state) {
+            return state.startNode;
+        },
+        getEndNode(state) {
+            return state.endNode;
+        },
+        getDraggedNode(state) {
+            return state.draggedNode;
+        },
+        getAnimState(state) {
+            return state.isAnimRunning;
+        },
+        getIfResultsDisplayed(state) {
+            return state.isResultsDisplayed;
+        },
+        getIfDropdownDisplayed(state) {
+            return state.isDropdownDisplayed;
+        },
     },
-    getEndNode(state) {
-      return state.endNode;
+    mutations: {
+        setNewStartNode(state, payload) {
+            state.startNode.row = payload.row;
+            state.startNode.col = payload.col;
+        },
+        setNewEndNode(state, payload) {
+            state.endNode.row = payload.row;
+            state.endNode.col = payload.col;
+        },
+        setDraggedNode(state, payload) {
+            state.draggedNode = payload.draggedNode;
+        },
+        setAnimState(state, payload) {
+            state.isAnimRunning = payload.isAnimRunning;
+        },
+        setIfResultsDisplayed(state, payload) {
+            state.isResultsDisplayed = payload.isResultsDisplayed;
+        },
+        setIfDropdownDisplayed(state, payload) {
+            state.isDropdownDisplayed = payload.setIfDropdownDisplayed;
+        },
     },
-    getDraggedNode(state) {
-      return state.draggedNode;
+    actions: {
+        changeStartNode(context, payload) {
+            context.commit("setNewStartNode", { row: payload.row, col: payload.col });
+        },
+        changeEndNode(context, payload) {
+            context.commit("setNewEndNode", { row: payload.row, col: payload.col });
+        },
+        changeDraggedNode(context, payload) {
+            context.commit("setDraggedNode", { draggedNode: payload });
+        },
+        animRun(context) {
+            context.commit("setAnimState", { isAnimRunning: true });
+        },
+        animEnd(context) {
+            context.commit("setAnimState", { isAnimRunning: false });
+        },
+        toggleAnimResults(context, payload) {
+            context.commit("setIfResultsDisplayed", { isResultsDisplayed: payload });
+        },
+        toggleDropdown(context, payload) {
+            context.commit("setIfDropdownDisplayed", {
+                setIfDropdownDisplayed: payload,
+            });
+        },
     },
-    getAnimState(state) {
-      return state.isAnimRunning;
-    },
-    getIfResultsDisplayed(state) {
-      return state.isResultsDisplayed;
-    },
-    getIfDropdownDisplayed(state) {
-      return state.isDropdownDisplayed;
-    },
-  },
-  mutations: {
-    setNewStartNode(state, payload) {
-      state.startNode.row = payload.row;
-      state.startNode.col = payload.col;
-    },
-    setNewEndNode(state, payload) {
-      state.endNode.row = payload.row;
-      state.endNode.col = payload.col;
-    },
-    setDraggedNode(state, payload) {
-      state.draggedNode = payload.draggedNode;
-    },
-    setAnimState(state, payload) {
-      state.isAnimRunning = payload.isAnimRunning;
-    },
-    setIfResultsDisplayed(state, payload) {
-      state.isResultsDisplayed = payload.isResultsDisplayed;
-    },
-    setIfDropdownDisplayed(state, payload) {
-      state.isDropdownDisplayed = payload.setIfDropdownDisplayed;
-    },
-  },
-  actions: {
-    changeStartNode(context, payload) {
-      context.commit("setNewStartNode", { row: payload.row, col: payload.col });
-    },
-    changeEndNode(context, payload) {
-      context.commit("setNewEndNode", { row: payload.row, col: payload.col });
-    },
-    changeDraggedNode(context, payload) {
-      context.commit("setDraggedNode", { draggedNode: payload });
-    },
-    animRun(context) {
-      context.commit("setAnimState", { isAnimRunning: true });
-    },
-    animEnd(context) {
-      context.commit("setAnimState", { isAnimRunning: false });
-    },
-    toggleAnimResults(context, payload) {
-      context.commit("setIfResultsDisplayed", { isResultsDisplayed: payload });
-    },
-    toggleDropdown(context, payload) {
-      context.commit("setIfDropdownDisplayed", {
-        setIfDropdownDisplayed: payload,
-      });
-    },
-  },
 });
